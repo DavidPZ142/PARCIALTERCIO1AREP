c0d81d8af3013a12c70cb54f77cab8f5a05026f6
DavidPZ666 <davidpz616@gmail.com>
2022-02-23 16:36:29 -0500

    a

--- src/main/java/edu/escuelaing/arem/HTPPServer.java
+++ src/main/java/edu/escuelaing/arem/HTPPServer.java
@@ -53,6 +53,13 @@ public class HTPPServer {
 
     }
 
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set
+    }
+
 }
 
 
